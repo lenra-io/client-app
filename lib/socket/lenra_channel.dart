41e0a1ba23f37e486544287a6daf7e8f4f249c04
Jonas Martinez <36544012+jonas-martinez@users.noreply.github.com>
2022-06-07 14:24:19 +0200

    fix: Fix lenrachannel (#3)

--- lib/socket/lenra_channel.dart
+++ lib/socket/lenra_channel.dart
@@ -8,45 +8,45 @@ class LenraChannel {
   LenraChannel(PhoenixSocket socket, String channelName, Map<String, dynamic> params) {
     _channel = socket.channel(channelName, params);
     _channel.join()?.receive("error", (Map<dynamic, dynamic>? response) {
-      _errorCallbacks.forEach((c) {
+      for (var c in _errorCallbacks) {
         c(response);
-      });
+      }
     });
   }
 
   void close() {
-    this._channel.off("ui");
-    this._channel.off("patchUi");
-    this._channel.off("error");
+    _channel.off("ui");
+    _channel.off("patchUi");
+    _channel.off("error");
     _channel.leave();
   }
 
   void onUi(void Function(Map<dynamic, dynamic>) callback) {
-    this._channel.on("ui", (payload, ref, joinRef) {
+    _channel.on("ui", (payload, ref, joinRef) {
       if (payload == null) return;
       callback(payload);
     });
   }
 
   void onPatchUi(void Function(Map<dynamic, dynamic>) callback) {
-    this._channel.on("patchUi", (payload, ref, joinRef) {
+    _channel.on("patchUi", (payload, ref, joinRef) {
       if (payload == null) return;
       callback(payload);
     });
   }
 
   void onAppError(void Function(Map<dynamic, dynamic>) callback) {
-    this._channel.on("error", (payload, ref, joinRef) {
+    _channel.on("error", (payload, ref, joinRef) {
       if (payload == null) return;
       callback(payload);
     });
   }
 
   void onError(dynamic Function(Map<dynamic, dynamic>?) callback) {
-    this._errorCallbacks.add(callback);
+    _errorCallbacks.add(callback);
   }
 
   void send(String event, dynamic data) {
-    this._channel.push(event: event, payload: data as Map<dynamic, dynamic>);
+    _channel.push(event: event, payload: data as Map<dynamic, dynamic>);
   }
 }
