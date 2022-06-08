2664852b96658cd4c2f0236951b24814e1391925
jonas-martinez <cbobbxta.fr@gmail.com>
2022-06-08 16:27:40 +0200

    need to fix the timer problem

--- test/lenra_ui_controller_test.dart
+++ test/lenra_ui_controller_test.dart
@@ -1,11 +1,18 @@
+import 'dart:convert';
+
 import 'package:client_app/lenra_ui_controller.dart';
+import 'package:client_app/models/channel_model.dart';
 import 'package:client_common/test/lenra_page_test_help.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:lenra_ui_runner/components/events/data/empty_data.dart';
+import 'package:lenra_ui_runner/components/events/data/value_data.dart';
 import 'package:lenra_ui_runner/components/events/event.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  testWidgets('LenraUiController simple instantiation', (WidgetTester tester) async {
+  testWidgets('LenraUiController simple instantiation',
+      (WidgetTester tester) async {
     await tester.runAsync(
       () => tester.pumpWidget(
         createAppTestWidgets(
@@ -23,11 +30,15 @@ void main() {
     expect(find.byType(CircularProgressIndicator), findsOneWidget);
   });
 
-  testWidgets('LenraUiController with initialized ChannelModel', (WidgetTester tester) async {
+  testWidgets('LenraUiController with initialized ChannelModel',
+      (WidgetTester tester) async {
+    final GlobalKey key = GlobalKey();
+
     await tester.runAsync(
       () => tester.pumpWidget(
         createAppTestWidgets(
-          const LenraUiController(
+          LenraUiController(
+            key: key,
             accessToken: "random-access-token",
             appName: "app-name",
           ),
@@ -35,10 +46,12 @@ void main() {
       ),
     );
 
-    await tester.pump(const Duration(seconds: 15));
+    await tester.pump();
 
-    Notification notification = Event(code: "code", data: "data");
-    
+    final context = tester.element(find.byType(Scaffold));
+    context.read<ChannelModel>().channel!.send("ui", {"data": "data"});
+
+    await tester.pump();
 
     expect(find.byType(LenraUiController), findsOneWidget);
     // expect(find.byType(LenraErrorPage), findsOneWidget);
